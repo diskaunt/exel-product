31ca96045c287fc2e29ee158d5ee6368c2fa66ea
DS <130369085+diskaunt@users.noreply.github.com>
2024-01-16 11:41:49 +0300

    Finish base framework functional (#3)
    Co-authored-by: Mikhail B <diskauntm@gmail.com.com>

---
+++ src/components/formula/Formula.js
@@ -0,0 +1,28 @@
+import {ExcelComponents} from '../../core/ExcelComponent';
+
+export class Formula extends ExcelComponents {
+static ClassName = 'excel__formula';
+
+constructor($root) {
+  super($root, {
+    name: 'Formula',
+    listeners: ['input', 'click'],
+  });
+}
+
+toHTML() {
+  return `
+	<div class="info">fx</div>
+					<div class="input" contenteditable="" spellcheck="false"></div>
+	`;
+}
+
+onInput(event) {
+  console.log(this.$root);
+  console.log('Formula: onInput', event);
+}
+
+onClick(event) {
+  console.log('click');
+}
+}

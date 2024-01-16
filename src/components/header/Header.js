82a820f87d34b321bff3c629747b5b9a682c19b3
Mikhail B <diskauntm@gmail.com.com>
2024-01-16 10:28:31 +0300

    Finish base framework functional

---
+++ src/components/header/Header.js
@@ -0,0 +1,25 @@
+import {ExcelComponents} from '../../core/ExcelComponent';
+
+export class Header extends ExcelComponents {
+static ClassName = 'excel__header';
+
+toHTML() {
+  return `
+<input type="text" class="input" value="Новая таблица"/>
+
+<div>
+
+<div class="button">
+<i class="material-icons">
+delete
+</i>
+</div>
+
+<div class="button">
+<i class="material-icons">
+exit_to_app
+</i>
+</div>
+`;
+}
+}

43a28075ca431504e70d4443755c0a853fd37308
Mikhail B <diskauntm@gmail.com.com>
2024-02-22 14:35:28 +0300

    Routing implemented

--- src/components/header/Header.js
+++ src/components/header/Header.js
@@ -3,6 +3,7 @@ import {changeTitle} from '../../redux/actions';
 import {$} from '../../core/dom';
 import {defaultTitle} from '../../constans';
 import {debounce} from '../../core/utils';
+import {ActiveRoute} from '../../core/routes/ActiveRoute';
 
 export class Header extends ExcelComponents {
 static ClassName = 'excel__header';
@@ -10,7 +11,7 @@ static ClassName = 'excel__header';
 constructor($root, options) {
   super($root, {
     name: 'Header',
-    listeners: ['input'],
+    listeners: ['input', 'click'],
     ...options,
   });
 }
@@ -26,18 +27,32 @@ toHTML() {
 
 <div>
 
-<div class="button">
-<i class="material-icons">
+<div class="button" data-button="remove">
+<i class="material-icons" data-button="remove">
 delete
 </i>
 </div>
 
-<div class="button">
-<i class="material-icons">
+<div class="button" data-button="exit">
+<i class="material-icons" data-button="exit">
 exit_to_app
 </i>
 </div>
 `;
+}
+
+onClick(event) {
+  const $target = $(event.target);
+
+  if ($target.data.button === 'remove') {
+    const deсision = confirm('Вы действительно хотите удальть эту таблицу?');
+    if (deсision) {
+      localStorage.removeItem('excel:' + ActiveRoute.param);
+      ActiveRoute.navigate('');
+    }
+  } else if ($target.data.button === 'exit') {
+    ActiveRoute.navigate('');
+  }
 }
 
 onInput(event) {

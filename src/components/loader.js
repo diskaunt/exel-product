85a20d82a04ade923711e00fe930bb20529e7244
Mikhail B <diskauntm@gmail.com>
2024-03-04 17:02:14 +0300

    Implement DIP

---
+++ src/components/loader.js
@@ -0,0 +1,19 @@
+import {$} from '../core/dom';
+
+export function loader() {
+  return $.create('div', 'loader').html(
+      `<div class="lds-spinner">
+        <div></div>
+        <div></div>
+        <div></div>
+        <div></div>
+        <div></div>
+        <div></div>
+        <div></div>
+        <div></div>
+        <div></div>
+        <div></div>
+        <div></div>
+        <div></div>
+      </div>`);
+}
